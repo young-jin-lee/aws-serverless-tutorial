035c27e2e4c56f48833acfbfadf4060af74ec662
EC2 Default User <ec2-user@ip-172-31-40-40.ec2.internal>
2020-07-02 06:31:54 +0000

    region added

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_kXvzVpvHv', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '5d4iqauvvp7eg4epnu2nm9hntt', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',

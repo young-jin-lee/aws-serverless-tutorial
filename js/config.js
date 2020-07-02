23a76093f7368d4e2aa1e12ad5f1d5a878b141f5
EC2 Default User <ec2-user@ip-172-31-40-40.ec2.internal>
2020-07-02 07:03:57 +0000

    invoke url updated

--- js/config.js
+++ js/config.js
@@ -2,9 +2,9 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_kXvzVpvHv', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '5d4iqauvvp7eg4epnu2nm9hntt', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+       region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://ddngeeywud.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };

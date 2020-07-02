7b1e1cc7391f1f61e0eb916fc33cf569e411230b
EC2 Default User <ec2-user@ip-172-31-40-40.ec2.internal>
2020-07-02 06:26:48 +0000

    added poolId and appClientId

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_kXvzVpvHv', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5d4iqauvvp7eg4epnu2nm9hntt', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
